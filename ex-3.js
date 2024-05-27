3284079f366edadb8624ffe98fe18d5edcf8f0ed
revieww12 <review120244@gmail.com>
2024-05-27 13:47:49 +0700

    feat: add code asynchronous in ex-1 to ex-6.

--- ex-3.js
+++ ex-3.js
@@ -33,3 +33,15 @@ let getJohnOrders = () => {
 };
 
 // Start coding here
+function onSuccess(data) {
+  console.log(data);
+}
+function onFailure(error) {
+  console.log(error);
+}
+getJohnProfile()
+  .then(onSuccess)
+  .catch(onFailure)
+  .then(getJohnOrders)
+  .then(onSuccess)
+  .catch(onFailure);
